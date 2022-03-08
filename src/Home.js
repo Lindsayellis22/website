9b4e40c68d13cfa66a1e58e4c718570d8108c589
lindsayellis22 <lindsayellis39@gmail.com>
2022-03-08 13:52:48 -0500

    add font

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,7 @@ function Home(props) {
       alt="First slide"
     />
     <Carousel.Caption>
-      <h5>First slide label</h5>
+      <h5>Records Matter<span>&nbsp;</span></h5>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
